a5cb59fe4b85d8c8fd14a8c41d1da8b6731bc4a5
wangconglin <wangconglin87@163.com>
2024-03-10 10:09:50 +0800

    添加wifi smart config

--- esp32/main/main.c
+++ esp32/main/main.c
@@ -15,7 +15,9 @@
 #include "freertos/task.h"
 #include "freertos/event_groups.h"
 
+#include "nvs.h"
 #include "nvs_flash.h"
+#include "esp_system.h"
 #include "esp_netif.h"
 #include "esp_err.h"
 #include "esp_event.h"
@@ -33,6 +35,8 @@
 #include "driver/rmt_encoder.h"
 
 #include "mqtt_client.h"
+#include "esp_smartconfig.h"
+#include "driver/gpio.h"
 
 #define IR_RESOLUTION_HZ 1000000 // 1MHz resolution, 1 tick = 1us
 #define IR_TX_GPIO_NUM 18
@@ -99,6 +103,10 @@ static const char *TAG = "My RMT";
 static int s_retry_num = 0;
 static rmt_channel_handle_t tx_channel = NULL;
 static rmt_encoder_handle_t gree_encoder = NULL;
+
+static EventGroupHandle_t s_wifi_event_group;
+static const int NETWORK_CONFIGED_BIT = BIT0;
+static const int ESPTOUCH_DONE_BIT = BIT1;
 
 static size_t rmt_encode_ir_gree(rmt_encoder_t *encoder,
                                  rmt_channel_handle_t channel,
@@ -540,6 +548,23 @@ static void mqtt_app_start(void)
     ESP_ERROR_CHECK(esp_mqtt_client_start(client));
 }
 
+static void smartconfig_task(void *parm)
+{
+    EventBits_t uxBits;
+    ESP_ERROR_CHECK(esp_smartconfig_set_type(SC_TYPE_ESPTOUCH));
+    smartconfig_start_config_t cfg = SMARTCONFIG_START_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_smartconfig_start(&cfg));
+    while (1)
+    {
+        uxBits = xEventGroupWaitBits(s_wifi_event_group, ESPTOUCH_DONE_BIT, true, false, portMAX_DELAY);
+        if (uxBits & ESPTOUCH_DONE_BIT)
+        {
+            esp_smartconfig_stop();
+            vTaskDelete(NULL);
+        }
+    }
+}
+
 static void event_handler(void *event_handler_arg,
                           esp_event_base_t event_base,
                           int32_t event_id,
@@ -547,7 +572,15 @@ static void event_handler(void *event_handler_arg,
 {
     if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START)
     {
-        ESP_ERROR_CHECK(esp_wifi_connect());
+        EventBits_t uxBits = xEventGroupGetBits(s_wifi_event_group);
+        if (uxBits & NETWORK_CONFIGED_BIT)
+        {
+            ESP_ERROR_CHECK(esp_wifi_connect());
+        }
+        else
+        {
+            xTaskCreate(smartconfig_task, "smartconfig_task", 4096, NULL, 3, NULL);
+        }
     }
     else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED)
     {
@@ -574,6 +607,37 @@ static void event_handler(void *event_handler_arg,
 
         mqtt_app_start();
     }
+    else if (event_base == SC_EVENT && event_id == SC_EVENT_GOT_SSID_PSWD)
+    {
+        smartconfig_event_got_ssid_pswd_t *evt = (smartconfig_event_got_ssid_pswd_t *)event_data;
+        wifi_config_t wifi_config;
+        bzero(&wifi_config, sizeof(wifi_config_t));
+        memcpy(wifi_config.sta.ssid, evt->ssid, sizeof(wifi_config.sta.ssid));
+        memcpy(wifi_config.sta.password, evt->password, sizeof(wifi_config.sta.password));
+        wifi_config.sta.bssid_set = evt->bssid_set;
+        if (wifi_config.sta.bssid_set == true)
+        {
+            memcpy(wifi_config.sta.bssid, evt->bssid, sizeof(wifi_config.sta.bssid));
+        }
+        nvs_handle_t nvs_handle;
+        ESP_ERROR_CHECK(nvs_open("storage", NVS_READWRITE, &nvs_handle));
+        ESP_ERROR_CHECK(nvs_set_u8(nvs_handle, "isNetConfed", 1));
+        nvs_close(nvs_handle);
+        xEventGroupSetBits(s_wifi_event_group, NETWORK_CONFIGED_BIT);
+        ESP_ERROR_CHECK(esp_wifi_disconnect());
+        ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
+        esp_wifi_connect();
+    }
+}
+
+#define GPIO_INPUT_IO_0 GPIO_NUM_9
+#define GPIO_INPUT_PIN_SEL 1ULL << GPIO_INPUT_IO_0
+#define ESP_INTR_FLAG_DEFAULT 0
+
+static void IRAM_ATTR gpio_isr_handler(void *arg)
+{
+    uint32_t gpio_num = (uint32_t)arg;
+    ESP_LOGI(TAG, "Im %lu", gpio_num);
 }
 
 void app_main(void)
@@ -586,6 +650,37 @@ void app_main(void)
         ret = nvs_flash_init();
     }
     ESP_ERROR_CHECK(ret);
+
+    s_wifi_event_group = xEventGroupCreate();
+    nvs_handle_t nvs_handle;
+    ESP_ERROR_CHECK(nvs_open("storage", NVS_READWRITE, &nvs_handle));
+    uint8_t flag = 0;
+    ret = nvs_get_u8(nvs_handle, "isNetConfed", &flag);
+    if (ret == ESP_OK)
+    {
+        if (flag == 1)
+        {
+            xEventGroupSetBits(s_wifi_event_group, NETWORK_CONFIGED_BIT);
+        }
+    }
+    else
+    {
+        xEventGroupClearBits(s_wifi_event_group, NETWORK_CONFIGED_BIT);
+    }
+
+    if (flag == 1)
+    {
+        // 设置恢复初始参数的按键
+        ESP_ERROR_CHECK(gpio_set_direction(GPIO_NUM_6, GPIO_MODE_INPUT));
+        ESP_ERROR_CHECK(gpio_pullup_en(GPIO_NUM_6));
+        int level = gpio_get_level(GPIO_NUM_6);
+        if (level == 0)
+        {
+            nvs_set_u8(nvs_handle, "isNetConfed", 0);
+            xEventGroupClearBits(s_wifi_event_group, NETWORK_CONFIGED_BIT);
+        }
+    }
+    nvs_close(nvs_handle);
 
     ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
@@ -602,28 +697,32 @@ void app_main(void)
                                                ESP_EVENT_ANY_ID,
                                                &event_handler,
                                                NULL));
+    ESP_ERROR_CHECK(esp_event_handler_register(SC_EVENT,
+                                               ESP_EVENT_ANY_ID,
+                                               &event_handler,
+                                               NULL));
 
     ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
-    wifi_config_t wifi_config = {
-        .sta = {
-            .ssid = "",
-            .password = "",
-        }};
-    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
-
+    EventBits_t uxBits = xEventGroupGetBits(s_wifi_event_group);
+    if (uxBits & NETWORK_CONFIGED_BIT)
+    {
+        wifi_config_t wifi_config;
+        ESP_ERROR_CHECK(esp_wifi_get_config(WIFI_IF_STA, &wifi_config));
+    }
     ESP_ERROR_CHECK(esp_wifi_start());
 
     init_ir();
-    // while (1)
-    // {
-    //     // 阻塞5s
-    //     vTaskDelay(1000 / portTICK_PERIOD_MS);
-    //     const ir_gree_scan_code_t scan_code = {
-    //         .data1 = 0xaaaaaaaa,
-    //         .data2 = 0xaaaaaaaa,
-    //         .data3 = 0xaaaaaaaa,
-    //         .data4 = 0xaaaaaaaa,
-    //     };
-    //     ESP_ERROR_CHECK(rmt_transmit(tx_channel, gree_encoder, &scan_code, sizeof(scan_code), &transmit_config));
-    // }
+
+    gpio_config_t io_conf = {
+        .pin_bit_mask = 1ULL << GPIO_INPUT_IO_0,
+        .mode = GPIO_MODE_INPUT,
+        .intr_type = GPIO_INTR_NEGEDGE,
+        .pull_up_en = GPIO_PULLUP_ENABLE,
+        .pull_down_en = GPIO_PULLDOWN_DISABLE,
+    };
+    ESP_ERROR_CHECK(gpio_config(&io_conf));
+    // install gpio isr service
+    gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT);
+    // hook isr handler for specific gpio pin
+    gpio_isr_handler_add(GPIO_INPUT_IO_0, gpio_isr_handler, (void *)GPIO_INPUT_IO_0);
 }
